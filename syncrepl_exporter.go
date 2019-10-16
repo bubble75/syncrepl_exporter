7ab0266a297ed2df545b6ffe76c4d8e296fc504a
Vladislav Bogdanov <bubble.hoster-ok.com>
2019-10-16 21:06:45 +0000

    Refactor code a lot to automaticaally get masters' contextCSN and compare it to a local one.
    Also, drop entries_num metric as expensive and non-infomative

--- syncrepl_exporter.go
+++ syncrepl_exporter.go
@@ -23,26 +23,39 @@ var (
 			Help: "Value whether a connection to OpenLDAP has been successful",
 		})
 
+	openldapMasterUp = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "openldap_master_up",
+			Help: "Value whether a connection to master OpenLDAP has been successful",
+		})
+
+	syncLag = prometheus.NewGauge(
+		prometheus.GaugeOpts{
+			Name: "openldap_syncrepl_lag",
+			Help: "Replication lag comparing to a master in seconds",
+		})
+
 	syncCookie = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
 			Name: "openldap_contextCSN",
-			Help: "The first contextCSN sync cookie",
+			Help: "The contextCSN sync cookie on a slave",
 		},
 		[]string{"index"},
 	)
 
-	numEntries = prometheus.NewGauge(
+	syncMasterCookie = prometheus.NewGaugeVec(
 		prometheus.GaugeOpts{
-			Name: "openldap_entries_num",
-			Help: "Number of entries in directory",
-		})
+			Name: "openldap_master_contextCSN",
+			Help: "The contextCSN sync cookie on a master",
+		},
+		[]string{"index"},
+	)
 )
 
 // Config stores the exporter configuration.
 var Config = struct {
 	Ldap struct {
-		Host     string `default:"localhost"`
-		Port     string `default:"636"`
+		URI      string `default:"ldap://localhost"`
 		Basedn   string `default:"dc=example,dc=org"`
 		StartTLS bool   `default:"false"`
 		Bind     bool   `default:"false"`
@@ -50,6 +63,11 @@ var Config = struct {
 		Bindpass string `default:""`
 	}
 }{}
+
+var masterURI string
+var epoch int64
+var mepoch int64
+var firstrun bool
 
 func ymdToUnix(contextCSN string) (timestamp int64, label string) {
 	// This is a totally crude approach to set a well known base time to parse another date later
@@ -63,8 +81,7 @@ func ymdToUnix(contextCSN string) (timestamp int64, label string) {
 	return time.Unix(), label
 }
 
-// Actually collect values from ldap
-func csnWorker() {
+func ldapConnect(uri string) *ldap.Conn {
 	conf := &tls.Config{
 		InsecureSkipVerify: true,
 	}
@@ -74,29 +91,95 @@ func csnWorker() {
 
 	if Config.Ldap.StartTLS {
 		// Connect to host
-		l, err = ldap.Dial("tcp", Config.Ldap.Host+":"+Config.Ldap.Port)
+		l, err = ldap.DialURL(uri)
 		if err != nil {
-			log.Fatal(err)
+			log.Error(err)
+			return nil
 		}
-		defer l.Close()
 
 		// Reconnect with TLS
 		err = l.StartTLS(conf)
+		if err != nil {
+			l.Close()
+			log.Error(err)
+			return nil
+		}
 	} else {
-		l, err = ldap.DialTLS("tcp", Config.Ldap.Host+":"+Config.Ldap.Port, conf)
+		l, err = ldap.DialURL(uri)
+		if err != nil {
+			log.Error(err)
+			return nil
+		}
 	}
 
 	// Bind
 	if Config.Ldap.Bind {
-		err = l.Bind(Config.Ldap.Bindcn, Config.Ldap.Bindpass)
+		err = l.Bind(Config.Ldap.Binddn, Config.Ldap.Bindpass)
+		if err != nil {
+			l.Close()
+			log.Error(err)
+			return nil
+		}
 	}
 
-	if err != nil {
-		openldapUp.Set(0)
-		log.Fatal(err)
-	} else {
-		defer l.Close()
+	return l
+}
 
+func getMaster(l *ldap.Conn) {
+	var match bool
+
+	searchRequest := ldap.NewSearchRequest(
+		"cn=monitor", // The base dn to search
+		ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
+		"(&(namingContexts=*)(MonitorUpdateRef=*))",    // The filter to apply
+		[]string{"namingContexts", "monitorUpdateRef"}, // A list attributes to retrieve
+		nil,
+	)
+
+	basedn, err := ldap.ParseDN(Config.Ldap.Basedn)
+	if err != nil {
+		log.Errorln(err)
+		return
+	}
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		log.Errorln(err)
+	} else {
+		i := 0
+		newMasterURI := ""
+
+		for _, entry := range sr.Entries {
+			i++
+			match = false
+			for _, masterBasedn := range entry.GetAttributeValues("namingContexts") {
+				dn, err := ldap.ParseDN(masterBasedn)
+				if err == nil && dn.Equal(basedn) {
+					match = true
+				}
+			}
+			if match {
+				newMasterURI = entry.GetAttributeValue("monitorUpdateRef")
+
+				if masterURI == "" {
+					log.Infoln("Got master LDAP server URI", newMasterURI)
+				}
+				masterURI = newMasterURI
+			}
+		}
+
+		if newMasterURI == "" {
+			if masterURI != "" {
+				log.Infoln("Clearing master LDAP server URI", masterURI, "not available anymore")
+			}
+			masterURI = ""
+		}
+	}
+}
+
+// Actually collect values from ldap
+func getCSN(l *ldap.Conn) (int64, string) {
+	if l != nil {
 		searchRequest := ldap.NewSearchRequest(
 			Config.Ldap.Basedn, // The base dn to search
 			ldap.ScopeBaseObject, ldap.NeverDerefAliases, 0, 0, false,
@@ -113,34 +196,67 @@ func csnWorker() {
 			openldapUp.Set(1)
 			for _, entry := range sr.Entries {
 				for _, csn := range entry.GetAttributeValues("contextCSN") {
-					epoch, label := ymdToUnix(csn)
-					syncCookie.WithLabelValues(label).Set(float64(epoch))
+					return ymdToUnix(csn)
 				}
 			}
 		}
-		searchRequest = ldap.NewSearchRequest(
-			Config.Ldap.Basedn, // The base dn to search
-			ldap.ScopeWholeSubtree, ldap.NeverDerefAliases, 0, 0, false,
-			"(objectClass=*)", // The filter to apply
-			[]string{"dn"},    // A list attributes to retrieve
-			nil,
-		)
-
-		sr, err = l.Search(searchRequest)
-		if err != nil {
-			openldapUp.Set(0)
-			log.Errorln(err)
-		} else {
-			openldapUp.Set(1)
-			numEntries.Set(float64(len(sr.Entries)))
-		}
-
 	}
+
+	return 0, ""
 }
 
 func ldapWorker() {
+	var l *ldap.Conn
+	var ml *ldap.Conn
+	var label string
+
+	firstrun = true
+
 	for {
-		csnWorker()
+		l = ldapConnect(Config.Ldap.URI)
+
+		if l != nil {
+			openldapUp.Set(1)
+			getMaster(l)
+			epoch, label = getCSN(l)
+			syncCookie.WithLabelValues(label).Set(float64(epoch))
+		} else {
+			openldapUp.Set(0)
+		}
+
+		if masterURI != "" {
+			ml = ldapConnect(masterURI)
+
+			if ml != nil {
+				openldapMasterUp.Set(1)
+				mepoch, label = getCSN(ml)
+				syncMasterCookie.WithLabelValues(label).Set(float64(mepoch))
+				syncLag.Set(float64(mepoch - epoch))
+			} else {
+				openldapMasterUp.Set(0)
+			}
+		} else {
+			openldapMasterUp.Set(0)
+
+			if firstrun {
+				log.Infoln("Unable to get master URI")
+			}
+		}
+
+		if l != nil {
+			l.Close()
+			l = nil
+		}
+
+		if ml != nil {
+			ml.Close()
+			ml = nil
+		}
+
+		if firstrun {
+			firstrun = false
+		}
+
 		time.Sleep(60 * time.Second)
 	}
 }
@@ -148,8 +264,10 @@ func ldapWorker() {
 func init() {
 	// Register metrics
 	prometheus.MustRegister(syncCookie)
-	prometheus.MustRegister(numEntries)
+	prometheus.MustRegister(syncMasterCookie)
 	prometheus.MustRegister(openldapUp)
+	prometheus.MustRegister(openldapMasterUp)
+	prometheus.MustRegister(syncLag)
 }
 
 func main() {
